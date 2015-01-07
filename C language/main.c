a8c98de1731ca03f6a4ba31d18e574d13fb6c12c
FLYINGMONCKEY <huangweijun1001@gmail.com>
2015-01-07 11:00:24 +0800

    programing exercise

---
+++ C language/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  input
+//
+//  Created by HWJ on 15-1-7.
+//  Copyright (c) 2015 HWJ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+
+
+    return 0;
+}
